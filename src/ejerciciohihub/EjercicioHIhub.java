ad1859873e818cd4cfe83580341c7c822a01c866
aceve <aceve@DESKTOP-QASMM7M>
2021-04-21 14:16:07 -0600

    ejercicico que muestra mi nombre

--- src/ejerciciohihub/EjercicioHIhub.java
+++ src/ejerciciohihub/EjercicioHIhub.java
@@ -17,6 +17,7 @@ public class EjercicioHIhub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("josue isaac orozco constantino");
+        System.out.println("hola mundo");
     }
     
 }
